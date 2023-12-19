9705901fc6dbde0859bccc53535540518a91467a
WIlliam Dalton <daltonws22@gmail.com>
2023-12-19 13:22:58 -0500

    added files for POC

--- insurance_brd_azure_generator.py
+++ insurance_brd_azure_generator.py
@@ -3,38 +3,34 @@ import json
 
 def get_chat_response(stored_procedure, api_key):
     """
-    Sends a modified stored procedure input to the OpenAI ChatGPT model to request a BRD and retrieves the response.
+    Sends a modified stored procedure input to the OpenAI Azure model to request a BRD and retrieves the response.
     
     Parameters:
     - stored_procedure (str): The stored procedure input from the user.
     - api_key (str): The API key for authenticating with the OpenAI API.
 
     Returns:
-    - str: The generated BRD from the ChatGPT model.
+    - str: The generated BRD from the Azure model.
     """
-    # URL for the OpenAI API endpoint
-    url = "https://api.openai.com/v1/chat/completions"
+    # Updated URL for the OpenAI API endpoint (Azure)
+    url = "https://acam-int-demo.openai.azure.com/"
 
     # Predefined request phrase for BRD generation
     brd_request = "Generate a Business Requirements Document for the following Insurance Stored Procedure:"
-
-    # Combining the request phrase with the stored procedure input
-    #full_message = brd_request + "\n" + stored_procedure 
 
     full_message = brd_request + "\n" + stored_procedure 
 
     # Setting up the headers for the API request, including the authorization token
     headers = {
-        "Authorization": f"Bearer {api_key}",
+        "Authorization": f"Bearer {api_key}",  # Updated API key
         "Content-Type": "application/json"
     }
 
-    # Preparing the data payload for the API request
+    # Updated model identifier
     data = {
-        "model": "gpt-3.5-turbo",  # Model identifier
+        "model": "apt-35-turbo",  # Updated model name
         "messages": [{"role": "user", "content": full_message}],
         "temperature": 0.3
-
     }
 
     # Making the POST request to the API and handling the response
@@ -73,9 +69,10 @@ def main():
     Main function to run the chatbot interface for generating a BRD.
     """
     # API key for OpenAI (Note: Securely manage the API key in production)
-    OPENAI_API_KEY = "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"
+    #OPENAI_API_KEY = "de58119df4904cdb8ae17f150175edb7"
+    OPENAI_API_KEY =  "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"
 
-    print("ChatGPT: Hi, I am a chatbot. I will be assisting you on generating a business requirement document using your stored procedure.")
+    print("Azure: Hi, I am a chatbot. I will be assisting you on generating a business requirement document using your stored procedure.")
     while True:
         # Capturing the user's stored procedure input
         user_input = get_multiline_input("Please enter your insurance stored procedure (type 'END' on a new line when done):") #
@@ -84,7 +81,7 @@ def main():
             break
         # Generating the BRD based on the stored procedure input
         response = get_chat_response(user_input, OPENAI_API_KEY)
-        print(f"ChatGPT: {response}")
+        print(f"Azure: {response}")
 
 if __name__ == "__main__":
     main()
